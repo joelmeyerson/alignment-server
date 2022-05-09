fac82c3748043c7c6d9479131fedf8224a08fce4
Joel Meyerson <joel@Joels-MacBook-Air.local>
2022-05-09 10:50:06 -0400

    Update fetch commands in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,14 +25,18 @@ export default function AlignmentApp() {
   async function fetchAlignments() {
     await fetch(
       // "http://127.0.0.1:8000/api/alignment",
-      "http://0.0.0.0:5000/api/alignment",
-      // "https://secure-wildwood-12717.herokuapp.com/api/alignment",
+      // "http://0.0.0.0:5000/api/alignment",
+      "https://secure-wildwood-12717.herokuapp.com/api/alignment",
       {
         method: "GET",
         credentials: "same-origin",
       }
     )
-      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+
+        return response.json();
+      })
       .then((alignments) => {
         // store list of incomplete jobs
         let notComplete = alignments.filter((ali) => ali.job_complete === false);
@@ -66,8 +70,8 @@ export default function AlignmentApp() {
     } else {
       // POST to server
       // const response = await fetch("http://127.0.0.1:8000/addAlignment/", {
-        const response = await fetch("http://0.0.0.0:5000/addAlignment/", {
-        // const response = await fetch("https://secure-wildwood-12717.herokuapp.com/addAlignment/", {
+      // const response = await fetch("http://0.0.0.0:5000/addAlignment/", {
+      const response = await fetch("https://secure-wildwood-12717.herokuapp.com/addAlignment/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
