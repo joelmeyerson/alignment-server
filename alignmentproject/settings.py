da0948dcd1290441f98ab575d12f6ef5b1b4e05f
Joel Meyerson <joel@Joels-MacBook-Air.local>
2022-05-08 23:33:31 -0400

    Change STATIC_ROOT in settings.py

--- alignmentproject/settings.py
+++ alignmentproject/settings.py
@@ -148,7 +148,8 @@ STATICFILES_DIRS = (
 )
 
 # added
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
