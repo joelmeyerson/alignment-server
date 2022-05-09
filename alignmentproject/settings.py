26455d601b31cde8ca5f1ced6500b9f4af75eac6
Joel Meyerson <joel@Joels-MacBook-Air.local>
2022-05-08 23:18:33 -0400

    Rebuild frontend

--- alignmentproject/settings.py
+++ alignmentproject/settings.py
@@ -39,6 +39,12 @@ CORS_ORIGIN_WHITELIST = [
      'http://127.0.0.1:3000'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 # Application definition
 
 INSTALLED_APPS = [
