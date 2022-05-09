7501c042ff9830dd73d2e576054b6ed9df4d8722
Joel Meyerson <joel@Joels-MacBook-Air.local>
2022-05-09 09:18:01 -0400

    Update CORS config in fetch requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,8 @@ export default function AlignmentApp() {
   // fetch alignments
   async function fetchAlignments() {
     await fetch(
-      "http://127.0.0.1:8000/api/alignment",
+      // "http://127.0.0.1:8000/api/alignment",
+      "https://secure-wildwood-12717.herokuapp.com/api/alignment",
       {
         credentials: "same-origin",
       }
@@ -62,7 +63,8 @@ export default function AlignmentApp() {
       setError(true);
     } else {
       // POST to server
-      const response = await fetch("http://127.0.0.1:8000/addAlignment/", {
+      // const response = await fetch("http://127.0.0.1:8000/addAlignment/", {
+      const response = await fetch("https://secure-wildwood-12717.herokuapp.com/addAlignment/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
