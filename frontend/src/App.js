6d8fe4521d739da28a7c83559c97b7f03b3d30c7
Joel Meyerson <joel@Joels-MacBook-Air.local>
2022-05-09 10:17:35 -0400

    Update fetch commands in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,8 @@ export default function AlignmentApp() {
   async function fetchAlignments() {
     await fetch(
       // "http://127.0.0.1:8000/api/alignment",
-      "http://0.0.0.0:5000/api/alignment",
+      // "http://0.0.0.0:5000/api/alignment",
+      "https://secure-wildwood-12717.herokuapp.com/api/alignment",
       {
         method: "GET",
         headers: {
@@ -68,7 +69,8 @@ export default function AlignmentApp() {
     } else {
       // POST to server
       // const response = await fetch("http://127.0.0.1:8000/addAlignment/", {
-        const response = await fetch("http://0.0.0.0:5000/addAlignment/", {
+        // const response = await fetch("http://0.0.0.0:5000/addAlignment/", {
+        const response = await fetch("https://secure-wildwood-12717.herokuapp.com/addAlignment/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
